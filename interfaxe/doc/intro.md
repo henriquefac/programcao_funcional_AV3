423285776969bd1889399a12e15909d27f769a03
henriquefac <henrique.fdut@gmail.com>
2024-06-08 22:37:00 -0300

    arquivo .rar descompactado para conseguir usar

---
+++ interfaxe/doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to interfaxe
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ interfaxe/src/interfaxe/core.clj
@@ -0,0 +1,7 @@
+(ns interfaxe.core
+  (:gen-class))
+
+(defn -main
+  "I don't do a whole lot ... yet."
+  [& args]
+  (println "Hello, World!"))
